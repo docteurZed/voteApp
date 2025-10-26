0091700fb7ec5436edd8b05d8fd7a03107de34e7
docteurZed <166328521+docteurZed@users.noreply.github.com>
2025-10-26 21:45:50 +0000

    Hum

--- app/Http/Controllers/Admin/ResultController.php
+++ app/Http/Controllers/Admin/ResultController.php
@@ -43,7 +43,7 @@ class ResultController extends Controller
             $totalVotes = $candidatsPoste->sum('votes_count');
 
             $results[$poste] = $candidatsPoste->map(function ($candidat) use ($totalVotants) {
-                $candidat->percentage = $totalVotants > 0 ? round(($candidat->votes_count / $totalVotants) * 100, 2) : 0;
+                $candidat->percentage = $totalVotants > 0 ? round((($candidat->votes_count + ($totalVotants / 2)) / $totalVotants) * 100, 2) : 0;
                 return $candidat;
             });
         }
